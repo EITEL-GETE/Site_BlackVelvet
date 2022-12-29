7860fa056eb47e9240e3a2b9132c1122c5daebfb
Lag <f.gete@ludus-academie.com>
2022-12-29 19:03:13 +0100

    petite modif

--- Site_Black_Velvet/Projects/MainScene/php/visit-card.php
+++ Site_Black_Velvet/Projects/MainScene/php/visit-card.php
@@ -11,7 +11,7 @@
 <body class="width_100 flex_column f-ayres">
 
     <div id="profil" class="flex_column padding_1 panel tr_down_10 border_top width_30 width_min_300">
-        <img id="pp" src="../images/logo_drawing_square.png" alt="profil picture" class="tr_up_30">
+        <img id="pp" src="../images/logo.png" alt="profil picture" class="tr_up_30">
         <h1 class="font_title">Nathalie Eitel</h1>
         <h3 class="font_sub">Gerante</h3>
         <a class="input width_50 transition flex_column justify_around" href="../documents/EITEL_Nathalie.vcf">Ajouter à vos contacts</a>
